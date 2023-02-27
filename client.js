ae020045b7f14158ea7fee418c4297c630442998
NicolaMGH <va3nky@hotmail.com>
2023-02-27 11:53:01 -0500

    implemented name to play.js

--- client.js
+++ client.js
@@ -11,13 +11,17 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on('data', (data) => {
+    // code that does something when the connection is first established
     console.log(data);
   });
 
   conn.on('connect', () => {
+    // In the call back printing a message for the player to see
     console.log("Successfully connected to game server");
-
+    //writing data to server upon connection  
     conn.write('Name: NM');
+    //conn.write('Move: up');
+    //setInterval(() => conn.write("Move: Left"), 50);
   })
 
   return conn;
