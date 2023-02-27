e23e1e08d271e540ae58a42b38078e12d742b249
NicolaMGH <va3nky@hotmail.com>
2023-02-27 12:03:34 -0500

    implemented input handling

--- play.js
+++ play.js
@@ -1,6 +1,25 @@
-const { log } = require("console");
 const net = require("net");
 const connect = require("./client");
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
 
 // establishes a connection with the game server
 connect();
