fa4d1d4a8d5da3b6c13d38ebd872ab92e94087a4
NicolaMGH <va3nky@hotmail.com>
2023-02-27 12:16:02 -0500

    implemented movement

--- input.js
+++ input.js
@@ -1,11 +1,16 @@
+// Stores the active TCP connection object.
+let connection;
+
+
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function (conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
+  connection = conn;
   return stdin;
 };
 
@@ -14,7 +19,22 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
-
+  if (key === "w") {
+    connection.write("Move: up");
+    console.log("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+    console.log("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+    console.log("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+    console.log("Move: right");
+  }
   
 };
 
